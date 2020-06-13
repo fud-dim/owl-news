230a542eee1d8cb932f503e1c9aed9bd69bfff20
VitrukDF <VitrukDF@yandex.ru>
2020-06-13 21:13:26 +0300

    Started to create a palette & changed app location

---
+++ src/constants/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  palette: {
+    primary: {
+      main: '#FFDE03',
+      light: '#FFFF54',
+      dark: '#C7AD00',
+      contrastText: '#000000'
+    },
+    secondary: {
+      main: '#0336FF',
+      light: '#6F63FF',
+      dark: '#0008CA',
+      contrastText: '#FFFFFF'
+    },
+  },
+};
+
+export default theme;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-import App from './app';
+import App from './components/containers/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));

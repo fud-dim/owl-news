e3f2745fd6bb7f8c27eceb71d7a425fea1516f20
Andrey Seldyukov <a.seldukov@onpoint.ru>
2020-06-16 00:38:27 +0300

    Добавил запрос к апи. Карточки

---
+++ src/constants/api.ts
@@ -0,0 +1,12 @@
+const apiKey = '13f222d8b64a4edcbcade96cdae94ebd';
+const categories = {
+  business:'business',
+  entertainment:'entertainment',
+  general:'general',
+  health:'health',
+  science:'science',
+  sports:'sports',
+  technology:'technology',
+
+};
+export {apiKey, categories};
